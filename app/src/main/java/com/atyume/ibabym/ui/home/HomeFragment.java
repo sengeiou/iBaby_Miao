b58ce69fb27f3b6a5ba082491ea6a90d5f373563
yume <yume456@163.com>
2020-03-09 19:27:17 +0800

    add intent

--- app/src/main/java/com/atyume/ibabym/ui/home/HomeFragment.java
+++ app/src/main/java/com/atyume/ibabym/ui/home/HomeFragment.java
@@ -1,14 +1,17 @@
 package com.atyume.ibabym.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -16,8 +19,6 @@ import androidx.lifecycle.ViewModelProviders;
 import com.atyume.ibabym.R;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
 
 public class HomeFragment extends Fragment {
 
@@ -42,7 +43,54 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-
+        adm_editMiao = (Button)getActivity().findViewById(R.id.adm_editMiao);
+        adm_editMiao.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(),MiaoManage.class);
+                    startActivity(intent);
+                }
+            });
+        adm_editBaby = (Button)getActivity().findViewById(R.id.adm_editBaby);
+        adm_editBaby.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),EditBaby.class);
+                startActivity(intent);
+            }
+        });
+        adm_editExam = (Button)getActivity().findViewById(R.id.adm_editExam);
+        adm_editExam.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),EditBaby.class);
+                startActivity(intent);
+            }
+        });
+        adm_editExPr = (Button)getActivity().findViewById(R.id.adm_editExPr);
+        adm_editExPr.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),EditBaby.class);
+                startActivity(intent);
+            }
+        });
+        adm_editOrder = (Button)getActivity().findViewById(R.id.adm_editOrder);
+        adm_editOrder.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),EditBaby.class);
+                startActivity(intent);
+            }
+        });
+        adm_editHos = (Button)getActivity().findViewById(R.id.adm_editHos);
+        adm_editHos.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),EditBaby.class);
+                startActivity(intent);
+            }
+        });
         /*final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -52,4 +100,16 @@ public class HomeFragment extends Fragment {
         });*/
         return root;
     }
+
+    /*@Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        Button adm_editMiao = (Button)getActivity().findViewById(R.id.adm_editMiao);
+        adm_editMiao.setOnClickListener(new View.onClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(),"正在前往",Toast.LENGTH_LONG).show();
+            }
+        })
+    }*/
 }
---
+++ app/src/main/java/com/atyume/ibabym/ui/home/MiaoManage.java
@@ -0,0 +1,41 @@
+package com.atyume.ibabym.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.atyume.ibabym.MainActivity;
+import com.atyume.ibabym.R;
+
+public class MiaoManage extends AppCompatActivity {
+
+    private Button add_miao;
+    private Button modify_miao;
+    private Button delete_miao;
+    private Button select_miao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_miaomanage);
+        add_miao =(Button) findViewById(R.id.add_miao);
+        add_miao.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                new Thread(){
+                    @Override
+                    public void run() {
+                        super.run();
+                        Intent intent = new Intent(MiaoManage.this, null);
+                        startActivity(intent);//返回页面1
+                        finish();
+                        //  startActivity(intent);
+                    }
+                }.start();
+            }
+        });
+    }
+}
