44eff855679f5972fc0892ff400cf1b2e412897b
yume <yume456@163.com>
2020-03-13 12:46:36 +0800

    style change 2

--- app/src/main/java/com/atyume/ibabym/ui/home/EditExam.java
+++ app/src/main/java/com/atyume/ibabym/ui/home/EditExam.java
@@ -9,15 +9,10 @@ import com.qmuiteam.qmui.widget.QMUITopBar;
 
 public class EditExam extends AppCompatActivity {
 
-    QMUITopBar exam_topbar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editexam);
     }
-    protected void initTopBar(){
-        exam_topbar = (QMUITopBar) findViewById(R.id.exam_topbar);
-        exam_topbar.setTitle("新增体检套餐");
-    }
+
 }
--- app/src/main/java/com/atyume/ibabym/ui/home/EditHos.java
+++ app/src/main/java/com/atyume/ibabym/ui/home/EditHos.java
@@ -9,15 +9,10 @@ import com.qmuiteam.qmui.widget.QMUITopBar;
 
 public class EditHos extends AppCompatActivity {
 
-    /*QMUITopBar exam_topbar;*/
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edithos);
     }
-    /*protected void initTopBar(){
-        exam_topbar = (QMUITopBar) findViewById(R.id.exam_topbar);
-        exam_topbar.setTitle("新增医院信息");
-    }*/
+
 }
